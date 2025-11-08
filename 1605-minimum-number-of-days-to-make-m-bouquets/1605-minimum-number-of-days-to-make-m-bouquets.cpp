11d3e0112403772257429eb46dab7b6309add91b
SohamSawhney <160209322+SohamSawhney@users.noreply.github.com>
2025-11-08 17:45:14 +0530

    Time: 25 ms (37.72%), Space: 70.5 MB (45.63%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,43 +1,46 @@
 class Solution {
 public:
-bool possible(vector<int>&bloomDay,int day,int m,int k){
-    int n=bloomDay.size();
-    int count=0;
-    int noofb=0;
-    for(int i=0;i<n;i++){
-        if(bloomDay[i]<=day)
-        count ++;
-        else{
-            noofb+=(count/k);
+bool f(const vector<int>&bloomDay,int m,int k,int timelimit){
+//can i make 'm'  bouqets of k adjacent flowers 
+// in the  given time limit 
+int count=0;
+int bouqetcounter=0;
+for(int i=0;i<bloomDay.size();i++){     
+    if(bloomDay[i]<=timelimit){
+        count++;
+        if(count==k){
+            bouqetcounter++;
             count=0;
         }
     }
-    noofb+=(count/k);
-    return noofb>=m;
+    else{
+        count=0;
+    }
+}
+
+return bouqetcounter>=m;
+
 }
     int minDays(vector<int>& bloomDay, int m, int k) {
       
-        long long val = 1LL * m * k;
-        int n=bloomDay.size();
-        if(val>n)
-        return -1;
-        int mini=INT_MAX;
-        int maxi=INT_MIN;
-        for(int i=0;i<n;i++){
-            mini=min(mini,bloomDay[i]);
-            maxi=max(maxi,bloomDay[i]);
+        int s=*min_element(bloomDay.begin(),bloomDay.end());
+        int e=*max_element(bloomDay.begin(),bloomDay.end());
+        int ans=-1;
+        while(s<=e){      // n.(log(e-s))
+             int mid=s+(e-s)/2;
+             //can i make 'm'  bouqets of k adjacent flowers
+             // in the mid no. of days
+        if(f(bloomDay,m,k,mid)){
+            ans=mid;
+            e=mid-1;
         }
-        int low=mini;
-        int high=maxi;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(possible(bloomDay,mid,m,k)){
-                high=mid-1;
-            }
-            else{
-                low=mid+1;
-            }
+        else{
+            s=mid+1;
         }
-        return low;
+
+        }
+        return ans;
+       
     }
+
 };
