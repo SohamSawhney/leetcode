2531db8a0b5c3df204aed6292343ec1e79cf8f35
SohamSawhney <160209322+SohamSawhney@users.noreply.github.com>
2026-01-30 00:11:58 +0530

    Time: 0 ms (100%), Space: 15.2 MB (20.05%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,15 +1,17 @@
 // class Solution {
 // public:
 // int f(vector<vector<int>>&matrix,int i,int j, vector<vector<int>>&dp){
+   
 //     if(j<0 || j>=matrix[0].size()){
 //         return 1e8;
+//     }
+//          if(dp[i][j]!=-1){
+//         return dp[i][j];
 //     }
 //     if(i==0){
 //         return dp[i][j]=matrix[i][j];   // i==0
 //     }
-//     if(dp[i][j]!=-1){
-//         return dp[i][j];
-//     }
+    
 //     int u=matrix[i][j]+f(matrix,i-1,j,dp);
 //     int ld=matrix[i][j]+f(matrix,i-1,j-1,dp);
 //     int rd=matrix[i][j]+f(matrix,i-1,j+1,dp);
@@ -42,23 +44,37 @@ public:
         int n = matrix.size();
         int m = matrix[0].size();
 
-        vector<vector<int>> dp = matrix; // copy first row
+          vector<vector<int>>dp(n,vector<int>(m,-1));
+          for(int j=0;j<m;j++){
+            dp[0][j]=matrix[0][j];
+          }
+          for(int i=1;i<n;i++){
+            for(int j=0;j<m;j++){
+               
+              int u  = matrix[i][j] + dp[i-1][j];
 
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                int up = dp[i-1][j];
-                int ld = (j > 0) ? dp[i-1][j-1] : 1e8;
-                int rd = (j < m-1) ? dp[i-1][j+1] : 1e8;
+int ld = 1e8;
+if (j-1 >= 0) {
+    ld = matrix[i][j] + dp[i-1][j-1];
+}
 
-                dp[i][j] += min(up, min(ld, rd));
+int rd = 1e8;
+if (j+1 < m) {
+    rd = matrix[i][j] + dp[i-1][j+1];
+}
+
+dp[i][j] = min(u, min(ld, rd));
+
+
             }
-        }
+          }
+          int mini=dp[n-1][0];
+          for(int j=1;j<m;j++){
+            mini=min(mini,dp[n-1][j]);
+          }
 
-        int ans = 1e8;
-        for (int j = 0; j < m; j++) {
-            ans = min(ans, dp[n-1][j]);
-        }
+          return mini;
 
-        return ans;
+      
     }
 };
