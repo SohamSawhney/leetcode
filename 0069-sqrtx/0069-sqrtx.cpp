f198616a26d170858e47ad57f09747887a783d0d
SohamSawhney <160209322+SohamSawhney@users.noreply.github.com>
2026-06-09 02:06:12 +0530

    Time: 0 ms (100%), Space: 8.3 MB (99.99%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     int mySqrt(int x) {
-    int low=1;
-    int high=x;
-    while(low<=high){
-        long long mid=low+(high-low)/2;
-        long long val=mid*mid;
-        if(val<=x){
-            low=mid+1;
+        if(x<2){
+            return x;
         }
-        else{
-            high=mid-1;
+        int low=1;
+        int high=x/2;
+        int ans=0;
+        while(low<=high){
+            long long  mid=low+(high-low)/2;
+            if(mid*mid<=x){
+                 ans=mid;
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
         }
+        return ans;
+        
     }
-    return high;
-    }
-
 };
