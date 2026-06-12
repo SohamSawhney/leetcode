4329a57d9ba4a367ce64f2dea82a95dd214fbe92
SohamSawhney <160209322+SohamSawhney@users.noreply.github.com>
2026-06-12 10:44:18 +0530

    Time: 0 ms (100%), Space: 8.7 MB (11.76%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+double power(double x,long long n){
+    if(n==0){
+        return 1;
+    }
+    double A=power(x,n/2);
+    if(n%2==0){
+        return A*A;
+    }
+    else{
+        return A*A*x;
+    }
+}
+
+    double myPow(double x, int n) {
+        long long N=n;
+   if(n<0){
+    x=1/x;
+    N=-N;
+   }
+   return power(x,N);
+    }
+};
