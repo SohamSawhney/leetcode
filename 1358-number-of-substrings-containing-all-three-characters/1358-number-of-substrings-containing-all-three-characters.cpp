85a9cec50e559d7dff89385473acb4525c2f66f4
SohamSawhney <160209322+SohamSawhney@users.noreply.github.com>
2026-07-24 19:10:47 +0530

    Time: 5 ms (72.29%), Space: 11.2 MB (28.46%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int left=0;
-        vector<int>freq(3,0);
-        int res=0;
-        for(int right=0;right<s.length();right++){
-            freq[s[right]-'a']++;
+     int left=0;
+     int res=0;
+     vector<int>freq(3,0);
+     int right=0;
+     int n=s.length();
 
-            while(freq[0]>0 and freq[1]>0 and freq[2]>0){
-                res+=(s.length()-right);
-                freq[s[left]-'a']--;
-
-
-                left++;
-            }
-            
-        }
-        return res;
+     while(right<n){
+        freq[s[right]-'a']++;
         
+
+    while(freq[0]>0 and freq[1]>0 and freq[2]>0){
+        res+=(n-right);
+        freq[s[left]-'a']--;
+        left++;
+
+    }
+
+
+
+
+        right++;
+     }
+        
+
+        return res;
     }
 };
