a0d37beb5740f507c3a8aea8f775d81838a00aa1
SohamSawhney <160209322+SohamSawhney@users.noreply.github.com>
2026-07-26 18:39:17 +0530

    Time: 23 ms (52.94%), Space: 70.6 MB (12.49%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -3,39 +3,45 @@ public:
  bool f(const vector<int>& bloomDay,int m,int k,int time){
     int count=0;
     int bloomcount=0;
-    for(int i=0;i<bloomDay.size();i++){
-        if(bloomDay[i]<=time){
-            count++;
-            if(count==k){
-                bloomcount++;
-                count=0;
-            }
+
+    for(int bouqets:bloomDay){
+        if(bouqets<=time){
+        count++;
+        if(count==k){
+            bloomcount++;
+            count=0;
+        }
         }
         else{
             count=0;
         }
+
     }
 
+  
 
 
-
-
-    return bloomcount>=m;
+     return bloomcount>=m;
+   
  }
     int minDays(vector<int>& bloomDay, int m, int k) {
         int low=*min_element(bloomDay.begin(),bloomDay.end());
         int high=*max_element(bloomDay.begin(),bloomDay.end());
-        int ans=-1;
+         int ans=-1;
+        // int mid=low+(high-low)/2;
         while(low<=high){
-         int mid=(low+high)/2;
-            if(f(bloomDay,m,k,mid)){
-                ans=mid;
-                high=mid-1;
-            }
-            else{
-                low=mid+1;
-            }
+                    int mid=low+(high-low)/2;
+        if(f(bloomDay,m,k,mid)){
+            ans=mid;
+            high=mid-1;
         }
-        return ans;
+        else{
+            low=mid+1;
+        }
+
+        }
+     
+
+     return ans;
     }
 };
