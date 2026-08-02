080297a1548b05aef05f9064ac63cfa085184983
SohamSawhney <160209322+SohamSawhney@users.noreply.github.com>
2026-08-02 16:33:57 +0530

    Time: 0 ms (100%), Space: 8.9 MB (38.25%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(char ch:s){
+            if(ch=='(' or ch=='[' or ch=='{'){
+                st.push(ch);
+            }
+            else{
+
+                if(st.empty()){
+                    return false;
+                }
+                if((ch==')' and st.top()!='(') or (ch==']' and st.top()!='[') or (ch=='}' and st.top()!='{')){
+                    return false;
+                }
+
+            st.pop();
+
+
+
+            }
+         
+        }
+        return st.empty();
+        
+    }
+};
