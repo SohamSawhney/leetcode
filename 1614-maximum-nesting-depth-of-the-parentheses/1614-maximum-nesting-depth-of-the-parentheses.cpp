ca22beb9b6452afc5fe84122853de512200be0e8
SohamSawhney <160209322+SohamSawhney@users.noreply.github.com>
2026-08-02 18:27:46 +0530

    Time: 0 ms (100%), Space: 8.4 MB (24.65%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        stack<char>st;
+        int ans=0;
+        for(char ch:s){
+            if(ch=='('){
+                st.push(ch);
+                ans=max(ans,(int)st.size());
+            }
+            else if(ch==')'){
+                st.pop();
+            }
+
+            
+        }
+        return ans;
+        
+    }
+};
