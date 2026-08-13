3c8ed5a9b850bb464889d955ab5fc048998410f6
SohamSawhney <160209322+SohamSawhney@users.noreply.github.com>
2026-08-14 01:42:57 +0530

    Time: 0 ms (100%), Space: 9.4 MB (45.35%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -2,21 +2,30 @@ class Solution {
 public:
 
    void solve(vector<string>&ans,int ind,string mapping[10],string output,string digits){
-    if(ind>=digits.length()){
-        ans.push_back(output);
+
+    if(ind>=digits.size()){
+     ans.push_back(output);
         return;
 
     }
+    
+
+
+
+
+
     int number=digits[ind]-'0';
     string value=mapping[number];
+
 
     for(int i=0;i<value.size();i++){
         output.push_back(value[i]);
         solve(ans,ind+1,mapping,output,digits);
         output.pop_back();
 
-
     }
+
+
 
    }
 
